1d9395d3c7838c8bd032cde80693ec2555dff442
Jürgen Pointinger <juergen.pointinger@gmail.com>
2021-02-07 16:20:56 +0100

    Check if matchMedia is supported

--- assets/js/2.0/color-scheme.js
+++ assets/js/2.0/color-scheme.js
@@ -2,12 +2,17 @@ document.addEventListener('DOMContentLoaded', setColorMode)
 window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', setColorMode);
 
 function setColorMode() {
-  let matched = window.matchMedia('(prefers-color-scheme: light)').matches;
-  if (matched) {
-    // light mode
-    document.documentElement.setAttribute('data-color-mode', 'light');
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+    let matched = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+    if (matched) {
+      // Light mode
+      document.documentElement.setAttribute('data-color-mode', 'light');
+    } else {
+      // Dark mode
+      document.documentElement.setAttribute('data-color-mode', 'dark');
+    }
   } else {
-    // dark mode
+    // Color scheme not support (use dark mode)
     document.documentElement.setAttribute('data-color-mode', 'dark');
-  }
+  }  
 }
