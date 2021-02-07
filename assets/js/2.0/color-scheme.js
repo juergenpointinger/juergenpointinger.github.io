5ce8d2bd65e0a7aafce9f1d8cbcacfafc9aa825e
Jürgen Pointinger <juergen.pointinger@gmail.com>
2021-02-07 09:59:17 +0100

    Switched color scheme default (dark)

--- assets/js/2.0/color-scheme.js
+++ assets/js/2.0/color-scheme.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', setColorMode)
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setColorMode);
+window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', setColorMode);
 
 function setColorMode() {
-  let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  let matched = window.matchMedia('(prefers-color-scheme: light)').matches;
   if (matched) {
-    //dark mode
-    document.documentElement.setAttribute('data-color-mode', 'dark');
-  } else {
-    //light mode
+    // light mode
     document.documentElement.setAttribute('data-color-mode', 'light');
+  } else {
+    // dark mode
+    document.documentElement.setAttribute('data-color-mode', 'dark');
   }
 }
